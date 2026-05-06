342d02189e58bd07f7c6880a1f2e8f1e096af391
Student <Student@PC-SC5035-10.utko.feec.vutbr.cz>
2026-05-06 17:30:05 +0200

    minor changes

--- lab4.py
+++ lab4.py
@@ -1,13 +1,22 @@
 import machine
 import time
 import aht20
-import os
+from generic.wifi import initialize_wifi
+from generic import config
+from generic.umqtt import implementation as mqtt
+from generic.my_print import my_print
 
 DEVICE_ADDR = 56
 period = 3000
+
+# LEDs
 led7 = machine.Pin(7, machine.Pin.OUT, value=0)
 led9 = machine.Pin(9, machine.Pin.OUT, value=0)
 led8 = machine.Pin(8, machine.Pin.OUT, value=0)
+
+# Buttons
+gpio20 = machine.Pin(20, machine.Pin.IN) # Pin.IN?
+gpio21 = machine.Pin(21, machine.Pin.IN)
 
 i2c_handle = machine.I2C(0, scl = 1, sda = 0, freq = 100000)
 #print(i2c_handle.scan())
@@ -15,30 +24,23 @@ i2c_handle = machine.I2C(0, scl = 1, sda = 0, freq = 100000)
 sensor = aht20.TMPSensor(i2c_handle, DEVICE_ADDR)
 sensor.measure()
 
-"""
-f = open('/sd/update.py', 'r')
-f_dest = open('/novyfw.py', 'w')
-data = f.read()
-f_dest.write(data)
-f_dest.close()
-
-f.close()"""
-
-#import novyfw
-
 while(1):
     sensor.measure()
     print(sensor.hum)
     print(sensor.temp)
 
+    # Switch Heating ON
     if sensor.temp > 24:
         led9.value(1)
+    # Switch Heating OFF
     else:
         led9.value(0)
 
+    
     if sensor.hum > 60:
         led8.value(1)
     else:
         led8.value(0)
+        
 
-    time.sleep_ms(2000)
+    time.sleep_ms(5000)
