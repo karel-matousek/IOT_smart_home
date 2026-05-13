961a523c41c0656d02310d65e6b4f49f245fab42
Student <Student@PC-SC5035-10.utko.feec.vutbr.cz>
2026-05-13 12:47:35 +0200

    functional mqtt connection

--- lab4.py
+++ lab4.py
@@ -1,46 +1,67 @@
 import machine
 import time
 import aht20
-from generic.wifi import initialize_wifi
-from generic import config
-from generic.umqtt import implementation as mqtt
-from generic.my_print import my_print
+from wifi import initialize_wifi
+import config
+import implementation as mqtt
+from my_print import my_print
 
 DEVICE_ADDR = 56
 period = 3000
 
-# LEDs
-led7 = machine.Pin(7, machine.Pin.OUT, value=0)
-led9 = machine.Pin(9, machine.Pin.OUT, value=0)
-led8 = machine.Pin(8, machine.Pin.OUT, value=0)
-
 # Buttons
-gpio20 = machine.Pin(20, machine.Pin.IN) # Pin.IN?
+gpio20 = machine.Pin(20, machine.Pin.IN)
 gpio21 = machine.Pin(21, machine.Pin.IN)
 
+# ADHT20
 i2c_handle = machine.I2C(0, scl = 1, sda = 0, freq = 100000)
 #print(i2c_handle.scan())
 
 sensor = aht20.TMPSensor(i2c_handle, DEVICE_ADDR)
 sensor.measure()
 
-while(1):
-    sensor.measure()
-    print(sensor.hum)
-    print(sensor.temp)
+# Constants for MQTT Topics
+MQTT_TOPIC_LAMP = 'home_iot/lamp'
+MQTT_TOPIC_TEMPERATURE = 'home_iot/temperature'
+MQTT_TOPIC_HUMIDITY = 'home_iot/humidity'
+MQTT_TOPIC_ALARM = 'home_iot/alarm'
 
-    # Switch Heating ON
-    if sensor.temp > 24:
-        led9.value(1)
-    # Switch Heating OFF
+MQTT_CLIENT_ID = b"sensors_picow"
+
+try:
+    if not initialize_wifi(config.wifi_ssid, config.wifi_password):
+        my_print('Error connecting to the network... exiting program')
     else:
-        led9.value(0)
+        # Connect to MQTT broker, start MQTT client
+        client = mqtt.connect_mqtt(MQTT_CLIENT_ID)
+        my_print('Connected to MQTT broker successfully!')
 
-    
-    if sensor.hum > 60:
-        led8.value(1)
-    else:
-        led8.value(0)
-        
+        #client.set_callback(mqtt_received_callback)
+        #mqtt.subscribe(client, MQTT_TOPIC_LAMP)
+        #mqtt.subscribe(client, MQTT_TOPIC_TEMPERATURE)
+        while True:
+            client.check_msg()
+            #client.publish('mode_iot/secret', 'send dudes')
+            
+            sensor.measure()
+            client.publish(MQTT_TOPIC_TEMPERATURE, str(sensor.temp))
+            client.publish(MQTT_TOPIC_HUMIDITY, str(sensor.hum))
+            #print(sensor.hum)
+            #print(sensor.temp)
+            
+            if gpio20.value():
+                client.publish(MQTT_TOPIC_LAMP, "OFF")
+            else:
+                client.publish(MQTT_TOPIC_LAMP, "ON")
+                
+                
+            if gpio21.value():
+                client.publish(MQTT_TOPIC_ALARM, "OFF")
+            else:
+                client.publish(MQTT_TOPIC_ALARM, "ON")
+                
+            
+            time.sleep_ms(1000)
 
-    time.sleep_ms(5000)
+except Exception as e:
+    my_print('Error:', e)
